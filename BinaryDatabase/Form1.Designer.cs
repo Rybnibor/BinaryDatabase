60ab0b2a10888490fe03f04af8e96461154d91b0
Marek Bialozyt <marek.bialozyt@outlook.com>
2015-08-16 18:16:24 +0200

    WinForm-BinaryDatabase
    The program in C# - in the Polish language version. The binary database
    that stores personal information. The content of the database is stored
    in a binary file. The database is specifically limited to handle up to
    10 people. Adding eleven people overwrites the data for the person
    entered at the earliest. The application allows you to add / delete /
    view and modify data. The application allows you to import and export
    data to a text file.

---
+++ BinaryDatabase/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace BinaryDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Imie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nazwisko = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataUrodzenia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.StanCywilny = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wczytajDaneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszZmianyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importujDaneZTxtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eksportDoTxtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Imie,
+            this.Nazwisko,
+            this.DataUrodzenia,
+            this.StanCywilny});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(483, 244);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Imie
+            // 
+            this.Imie.HeaderText = "Imie";
+            this.Imie.Name = "Imie";
+            this.Imie.ReadOnly = true;
+            // 
+            // Nazwisko
+            // 
+            this.Nazwisko.HeaderText = "Nazwisko";
+            this.Nazwisko.Name = "Nazwisko";
+            this.Nazwisko.ReadOnly = true;
+            this.Nazwisko.Width = 150;
+            // 
+            // DataUrodzenia
+            // 
+            this.DataUrodzenia.HeaderText = "DataUrodzenia";
+            this.DataUrodzenia.Name = "DataUrodzenia";
+            this.DataUrodzenia.ReadOnly = true;
+            // 
+            // StanCywilny
+            // 
+            this.StanCywilny.HeaderText = "StanCywilny";
+            this.StanCywilny.Name = "StanCywilny";
+            this.StanCywilny.ReadOnly = true;
+            this.StanCywilny.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.StanCywilny.Width = 80;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dodaj wprowadzoną osobę";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(546, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Wczytaj zaznaczoną pozycję";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wczytajDaneToolStripMenuItem,
+            this.zapiszZmianyToolStripMenuItem,
+            this.importujDaneZTxtToolStripMenuItem,
+            this.eksportDoTxtToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // wczytajDaneToolStripMenuItem
+            // 
+            this.wczytajDaneToolStripMenuItem.Name = "wczytajDaneToolStripMenuItem";
+            this.wczytajDaneToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.wczytajDaneToolStripMenuItem.Text = "Wczytaj dane";
+            this.wczytajDaneToolStripMenuItem.Click += new System.EventHandler(this.wczytajDaneToolStripMenuItem_Click);
+            // 
+            // zapiszZmianyToolStripMenuItem
+            // 
+            this.zapiszZmianyToolStripMenuItem.Name = "zapiszZmianyToolStripMenuItem";
+            this.zapiszZmianyToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.zapiszZmianyToolStripMenuItem.Text = "Zapisz zmiany";
+            this.zapiszZmianyToolStripMenuItem.Click += new System.EventHandler(this.zapiszZmianyToolStripMenuItem_Click);
+            // 
+            // importujDaneZTxtToolStripMenuItem
+            // 
+            this.importujDaneZTxtToolStripMenuItem.Name = "importujDaneZTxtToolStripMenuItem";
+            this.importujDaneZTxtToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.importujDaneZTxtToolStripMenuItem.Text = "Importuj dane z txt";
+            this.importujDaneZTxtToolStripMenuItem.Click += new System.EventHandler(this.importujDaneZTxtToolStripMenuItem_Click);
+            // 
+            // eksportDoTxtToolStripMenuItem
+            // 
+            this.eksportDoTxtToolStripMenuItem.Name = "eksportDoTxtToolStripMenuItem";
+            this.eksportDoTxtToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.eksportDoTxtToolStripMenuItem.Text = "Eksport do txt";
+            this.eksportDoTxtToolStripMenuItem.Click += new System.EventHandler(this.eksportDoTxtToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(545, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Imię";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(518, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nazwisko";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(490, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Data Urodzenia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(504, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Stan Cywilny";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(586, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(586, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(586, 120);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Wolny",
+            "Zajęty",
+            "Brak danych"});
+            this.comboBox1.Location = new System.Drawing.Point(586, 151);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(546, 227);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Wprowadź w zaznaczone miejsce";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(546, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(182, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Usuń zaznaczoną pozycję";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 292);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Binarna baza osób";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszZmianyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wczytajDaneToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Imie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nazwisko;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataUrodzenia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn StanCywilny;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStripMenuItem importujDaneZTxtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eksportDoTxtToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ BinaryDatabase/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryDatabase
+{
+    public partial class Form1 : Form
+    {
+        const string fileName = "data.dat";
+
+        public Form1()
+        {
+            InitializeComponent();
+            wczytajDaneToolStripMenuItem_Click(this,EventArgs.Empty);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.RowCount>9)
+            {
+                //MessageBox.Show("Wprowadzono za dużo pozycji, zostanie skasowana pozycja nr 1");
+                dataGridView1.Rows.Remove(dataGridView1.Rows[0]);
+                dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, dateTimePicker1.Text, comboBox1.Text);
+            }
+            else
+            {
+                dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, dateTimePicker1.Text, comboBox1.Text);
+            }
+        }
+
+
+        void WriteValues()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    writer.Write(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                    writer.Write(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                    writer.Write(dataGridView1.Rows[i].Cells[2].Value.ToString());
+                    writer.Write(dataGridView1.Rows[i].Cells[3].Value.ToString());
+                }
+                
+                 
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count>0)
+            {
+                textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                dateTimePicker1.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                comboBox1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                dataGridView1.CurrentRow.Cells[0].Value = textBox1.Text;
+                dataGridView1.CurrentRow.Cells[1].Value = textBox2.Text;
+                dataGridView1.CurrentRow.Cells[2].Value = dateTimePicker1.Text;
+                dataGridView1.CurrentRow.Cells[3].Value = comboBox1.Text;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+            }
+            
+        }
+
+        private void zapiszZmianyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            WriteValues();
+        }
+
+        private void wczytajDaneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            while (dataGridView1.SelectedCells.Count > 0)
+            {              
+                dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+            }
+           
+               if (File.Exists(fileName))
+               {
+                   try
+                   {
+                       using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                       {
+                           for (int i = 0; i < 10; i++)
+                           {
+                               dataGridView1.Rows.Add(reader.ReadString(), reader.ReadString(), reader.ReadString(), reader.ReadString());
+                           }
+                       }
+                   }
+                   catch (EndOfStreamException)
+                   {
+                       return;
+                   }
+            }
+        }
+
+        private void importujDaneZTxtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "All files (*.*)|*.*|Txt files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            while (dataGridView1.SelectedCells.Count > 0)
+                            {
+                                dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+                            }
+                            System.IO.StreamReader file = new System.IO.StreamReader(myStream);
+                            string read = "1";
+                            for (int i = 0; i < 10; i++)
+                            {
+                                read = file.ReadLine();
+                                if (read!=null)
+                                {
+                                    dataGridView1.Rows.Add(read, file.ReadLine(), file.ReadLine(), file.ReadLine());
+                                } 
+                            }
+                            file.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Błąd: Nie można było odczytać danych z dysku. Szczegóły: " + ex.Message);
+                }
+            }
+        }
+
+        private void eksportDoTxtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "All files (*.*)|*.*|Txt files (*.txt)|*.txt";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    using (StreamWriter sw = new StreamWriter(myStream))
+                    {
+                        for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                        {
+                            sw.WriteLine(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                            sw.WriteLine(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                            sw.WriteLine(dataGridView1.Rows[i].Cells[2].Value.ToString());
+                            sw.WriteLine(dataGridView1.Rows[i].Cells[3].Value.ToString());
+                        }
+
+                    }
+                    myStream.Close();
+                }
+            }
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Zapisać zmiany w bazie?", "Zamykanie programu", MessageBoxButtons.YesNoCancel);
+            if (dialog==DialogResult.Yes)
+            {
+                WriteValues();
+            }
+            if (dialog==DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
